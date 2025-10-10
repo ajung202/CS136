120a44a0fb4d208a71925a633abb4cbb3381f691
ajung202 <alex060131@gmail.com>
2025-10-09 23:17:21 -0400

    Initial Commit

---
+++ Comparisons.cs
@@ -0,0 +1,28 @@
+public class Program{
+  public static void Main(string[] args){
+    Student student1 = new Student (9145, "62 UN Village-gil", "Alex Jung");
+    Student student2 = new Student (2370, "33 Marine City 2-ro", "Inah Jung");
+    Student student3 = new Student (9145, "O-block", "Yeonho Jung");
+    Console.WriteLine(student1.Equals(student2));
+    Console.WriteLine(student1.Equals(student3));
+  }
+}
+
+public class Student {
+  int studentID;
+  string address;
+  string name;
+
+  public Student (int studentID, string address, string name){
+    this.studentID = studentID;
+    this.address = address;
+    this.name = name;
+  }
+
+  public override bool Equals(Object o){
+    if (o is Student student){
+      return (this.studentID == student.studentID);
+    }
+    return false;
+  }
+}
